45e74dca223a612f34fff5ea88d5b15e0727dc33
Martin Lugaliki <mlugaliki@gmail.com>
2024-01-23 21:19:15 +0300

    Bug fix

--- yoga/videos.php
+++ yoga/videos.php
@@ -26,7 +26,7 @@ class Videos{
     public function getVideoPlan($name){
         $database = new Database();
         $conn = $database->getConnection();
-        $sqlQuery = "SELECT vc.id, vc.category_name,vc.image,vc.video_id,v.video_name,fn_day FROM vd_category vc INNER JOIN video v ON vc.video_id =v.id WHERE v.fn_day=:fn_day ORDER BY fn_day ASC;";
+        $sqlQuery = "SELECT vc.id, vc.category_name,vc.image,vc.video_id,v.video_name,fn_day FROM vd_category vc INNER JOIN video v ON vc.video_id =v.id WHERE v.video_name=:name ORDER BY fn_day ASC";
         if($conn == null){
             echo ("<p>Invalid connection</p>");
         }
