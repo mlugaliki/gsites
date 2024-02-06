3f8373245e0fbc76c138fc37847fbb699ae3fa91
Martin Lugaliki <martin.lugaliki@equitybank.co.ke>
2024-02-06 11:05:19 +0300

    Updated code

---
+++ yoga/SubscriberService.php
@@ -0,0 +1,19 @@
+<?php
+
+class Subscribers
+{
+
+    public function __construct()
+    {
+    }
+
+    public function getSubscriber($msisdn, $service){
+
+    }
+
+    public function saveSubscriber($msisdn, $name, $service)
+    {
+        
+
+    }
+}
