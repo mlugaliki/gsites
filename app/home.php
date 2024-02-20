ec565e72c813b7e3516b03d17adc1cba6038bc00
Martin Lugaliki <martin.lugaliki@equitybank.co.ke>
2024-02-20 14:16:21 +0300

    Fixed endpoints

--- app/home.php
+++ app/home.php
@@ -1,5 +1,5 @@
 <?php
-  include 'app/register.php';
+  include 'register.php';
 ?>
 <!DOCTYPE html>
 <html lang="en" class=" ">
@@ -16,46 +16,46 @@
   <meta content="themepassion" name="author" />
 
   <!-- App Icons -->
-  <link rel="apple-touch-icon" sizes="57x57" href="../assets/images/icons/apple-icon-57x57.png" />
-  <link rel="apple-touch-icon" sizes="60x60" href="../assets/images/icons/apple-icon-60x60.png" />
-  <link rel="apple-touch-icon" sizes="72x72" href="../assets/images/icons/apple-icon-72x72.png" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/images/icons/apple-icon-76x76.png" />
-  <link rel="apple-touch-icon" sizes="114x114" href="../assets/images/icons/apple-icon-114x114.png" />
-  <link rel="apple-touch-icon" sizes="120x120" href="../assets/images/icons/apple-icon-120x120.png" />
-  <link rel="apple-touch-icon" sizes="144x144" href="../assets/images/icons/apple-icon-144x144.png" />
-  <link rel="apple-touch-icon" sizes="152x152" href="../assets/images/icons/apple-icon-152x152.png" />
-  <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/icons/apple-icon-180x180.png" />
-  <link rel="icon" type="image/png" sizes="192x192" href="../assets/images/icons/android-icon-192x192.png" />
-  <link rel="icon" type="image/png" sizes="512x512" href="../assets/images/icons/android-icon-512x512.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/icons/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="96x96" href="../assets/images/icons/favicon-96x96.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/icons/favicon-16x16.png" />
-  <link rel="manifest" href="../assets/images/icons/manifest.json" />
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/images/icons/apple-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/icons/apple-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/icons/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/icons/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/icons/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/icons/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/icons/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/icons/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-icon-180x180.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/icons/android-icon-192x192.png" />
+  <link rel="icon" type="image/png" sizes="512x512" href="assets/images/icons/android-icon-512x512.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/icons/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png" />
+  <link rel="manifest" href="assets/images/icons/manifest.json" />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="msapplication-TileImage" content="assets/images/icons/ms-icon-144x144.png" />
   <meta name="theme-color" content="#ffffff" />
 
   <!-- CORE CSS FRAMEWORK - START -->
-  <link href="../assets/css/preloader.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="assets/css/preloader.css" type="text/css" rel="stylesheet" media="screen" />
 
-  <link href="../modules/materialize/materialize.min.css" type="text/css" rel="stylesheet" media="screen" />
-  <link href="../modules/fonts/mdi/appicon/appicon.css" type="text/css" rel="stylesheet" media="screen" />
-  <link href="../modules/fonts/mdi/materialdesignicons.min.css" type="text/css" rel="stylesheet" media="screen" />
-  <link href="../modules/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/materialize/materialize.min.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/fonts/mdi/appicon/appicon.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/fonts/mdi/materialdesignicons.min.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen" />
 
   <!-- CORE CSS FRAMEWORK - END -->
 
   <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
-  <link href="../modules/fancybox/jquery.fancybox.min.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="modules/fancybox/jquery.fancybox.min.css" rel="stylesheet" type="text/css" media="screen" />
   <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
 
   <!-- CORE CSS TEMPLATE - START -->
 
-  <link href="../assets/css/style.css" type="text/css" rel="stylesheet" media="screen" id="main-style" />
+  <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen" id="main-style" />
   <!-- CORE CSS TEMPLATE - END -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="../css/production.css"/>
+    <link href="css/production.css"/>
 </head>
 <!-- END HEAD -->
 
@@ -185,7 +185,7 @@
             <div class="prod-img-wrap">
               <a class="img-wrap" href="https://wap.guruhub.tech/yoga/video/drumbell/day1/day1_1.mp4"
                 data-fancybox="images" data-caption="Home Decor Balls">
-                <img alt="image" class="z-depth-1" style="width: 100%;" src="../img/drumbell/banner.png">
+                <img alt="image" class="z-depth-1" style="width: 100%;" src="img/drumbell/banner.png">
               </a>
             </div>
             <div class="prod-info  boxed z-depth-1">
@@ -204,7 +204,7 @@
             <div class="prod-img-wrap">
               <a class="img-wrap" href="#" data-fancybox="images"
                 data-caption="Women Sandals">
-                <img alt="image" class="z-depth-1" style="width: 100%;" src="../img/edu.jpeg">
+                <img alt="image" class="z-depth-1" style="width: 100%;" src="img/edu.jpeg">
               </a>
             </div>
             <div class="prod-info  boxed z-depth-1">
@@ -223,7 +223,7 @@
             <div class="prod-img-wrap">
               <a class="img-wrap" href="#" data-fancybox="images"
                 data-caption="Modern Man Shoes">
-                <img alt="image" class="z-depth-1" style="width: 100%;" src="../img/commedy.jpeg">
+                <img alt="image" class="z-depth-1" style="width: 100%;" src="img/commedy.jpeg">
               </a>
             </div>
             <div class="prod-info  boxed z-depth-1">
@@ -251,19 +251,19 @@
     </div>
     <!--.content-area-->
       <?php include './pages/bottom-menu.php'; ?>
-    <script src="../assets/js/pwa.js"></script>
+    <script src="assets/js/pwa.js"></script>
 
     <!-- LOAD FILES AT PAGE END FOR FASTER LOADING -->
 
     <!-- CORE JS FRAMEWORK - START -->
-    <script src="../modules/jquery/jquery-2.2.4.min.js"></script>
-    <script src="../modules/materialize/materialize.js"></script>
-    <script src="../modules/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/variables.js"></script>
+    <script src="modules/jquery/jquery-2.2.4.min.js"></script>
+    <script src="modules/materialize/materialize.js"></script>
+    <script src="modules/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/variables.js"></script>
     <!-- CORE JS FRAMEWORK - END -->
 
     <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
-    <script src="../modules/fancybox/jquery.fancybox.min.js"></script>
+    <script src="modules/fancybox/jquery.fancybox.min.js"></script>
     <script type="text/javascript">
       $(document).ready(function () {
         $(".carousel-fullscreen.carousel-slider").carousel({
@@ -283,7 +283,7 @@
     </script>
 
     <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
-    <script src="../modules/fancybox/jquery.fancybox.min.js"></script>
+    <script src="modules/fancybox/jquery.fancybox.min.js"></script>
     <script type="text/javascript">
       $(".modal").modal();
       //  document.addEventListener("DOMContentLoaded", function () {
@@ -293,18 +293,18 @@
       // });
     </script>
 
-    <script src="../assets/js/common.js"></script>
+    <script src="assets/js/common.js"></script>
     <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
 
     <!-- CORE TEMPLATE JS - START -->
-    <script src="../modules/app/init.js"></script>
-    <script src="../modules/app/settings.js"></script>
+    <script src="modules/init.js"></script>
+    <script src="modules/settings.js"></script>
 
-    <script src="../modules/app/scripts.js"></script>
+    <script src="modules/scripts.js"></script>
 
     <!-- END CORE TEMPLATE JS - END -->
 
-    <script src="../assets/js/preloader.js"></script>
+    <script src="assets/js/preloader.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"></script>
