13b2b62f4a5fe9fa06788aec83f3b6c02bb337ee
Martin Lugaliki <martin.lugaliki@equitybank.co.ke>
2024-02-22 16:46:44 +0300

    HE implementation

---
+++ app/pages/footer.php
@@ -0,0 +1,38 @@
+<footer class="page-footer center primary-bg white-text footer-ex social-colored">
+<div class="container footer-content">
+    <div class="row">
+    <div class="">
+        <h5 class="logo">GURUHUB</h5>
+    </div>
+    <div class="link-wrap">
+        <ul class="link-ul">
+        <li>
+            <a class="" href="#!"><i class="mdi mdi-phone"></i> +254774812586</a>
+        </li>
+        <li>
+            <a class="" href="#!"><i class="mdi mdi-email"></i> info@guruhub.tech</a>
+        </li>
+        <li>
+            <a class="" href="#!"><i class="mdi mdi-map-marker"></i>Vision Plaza, Mombasa Road</a>
+        </li>
+        </ul>
+        <ul class="social-wrap">
+        <li class="social">
+            <a class="" href="#!"><i class="mdi mdi-facebook"></i></a>
+            <a class="" href="#!"><i class="mdi mdi-twitter"></i></a>
+            <a class="" href="#!"><i class="mdi mdi-dribbble"></i></a>
+            <a class="" href="#!"><i class="mdi mdi-google-plus"></i></a>
+            <a class="" href="#!"><i class="mdi mdi-linkedin"></i></a>
+        </li>
+        </ul>
+    </div>
+    </div>
+</div>
+
+<div class="footer-copyright">
+    <div class="container">
+    &copy; Copyright
+    <a class="" href="https://guruhub.tech">GIL</a>. All rights reserved.
+    </div>
+</div>
+</footer>
---
+++ app/test.php
@@ -0,0 +1,59 @@
+<?php
+include 'HttpUtilClient.php';
+
+// Get token
+$url = "https://dev-account-safaricom.auth.eu-west-1.amazoncognito.com/oauth2/token?grant_type=client_credentials";
+$headers = ['Content-type: application/x-www-form-urlencoded',
+    'Authorization: Basic MzNwZGE4ajBzNmd2YTBybG9vZzhmcG1kczU6NTduYWE0NjRwMGM2bGJiNWliOHZkMXA4MGtrMzFrdG5kcGNyNzBqZ3NzNmMwMW1nb2x2',
+];
+
+echo base64_encode("33pda8j0s6gva0rloog8fpmds5:57naa464p0c6lbb5ib8vd1p80kk31ktndpcr70jgss6c01mgolv")."\n";
+$he = new HttpUtilClient($url, $headers);
+$data = $he->getToken();
+if ($data != null && sizeof($data) > 0) {
+    // Get subscribers number
+    $token = trim($data['access_token']);
+    $headers = [
+        'X-App: he-partner',
+        'x-correlation-conversationid: 434',
+        'X-MessageID: 1234',
+        'X-DeviceId: 1234',
+        'X-DeviceToken: 1234',
+        'X-Version: 232',
+        'X-Source-System: he-partner',
+        'Authorization: Bearer ' . $token
+    ];
+
+    $url = "https://uat-identity.safaricom.com/partner/api/v2/fetchMaskedMsisdn";
+    $he = new HttpUtilClient($url, $headers);
+    $data = $he->getMaskedNumber();
+    $heData = array();
+    $heData['success'] = "true";
+    if ($data != null) {
+        $keyExists = array_key_exists('ServiceResponse', $data);
+        if (array_key_exists('ServiceResponse', $data)) {
+            $response = $data['ServiceResponse'];
+            if (array_key_exists('ResponseHeader', $response)) {
+                $header = $response['ResponseHeader'];
+                if (array_key_exists('ResponseCode', $header) && array_key_exists('ResponseMsg', $header)) {
+                    $heData['responseCode'] = $header['ResponseCode'];
+                    $heData['responseMsg'] = $header['ResponseMsg'];
+                }
+            }
+
+            if (array_key_exists('ResponseBody', $response)) {
+                $body = $response['ResponseBody'];
+                if (array_key_exists('Response', $body)) {
+                    $bodyResponse = $body['Response'];
+                    if (array_key_exists('Msisdn', $bodyResponse)) {
+                        $msisdn = $bodyResponse['Msisdn'];
+                        $heData['msisdn'] = $msisdn;
+                        $heData['success'] = !empty($msisdn) ? "true" : "false";
+                    }
+                }
+            }
+        }
+    }
+
+    print_r($heData);
+}
