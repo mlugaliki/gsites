ccd4844fe5463e6aa0296d4eca0202a931123123
Martin Lugaliki <martin.lugaliki@equitybank.co.ke>
2024-03-27 23:53:23 +0300

    Update

--- app/HttpUtilClient.php
+++ app/HttpUtilClient.php
@@ -6,16 +6,35 @@ class HttpUtilClient
     {
         //$url = "http://159.65.208.160:8480/vasmasta/he/find-number";
         $url = "http://127.0.0.1:8080/vasmasta/he/find-number";
-        try
-        {
+        try {
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
             $resp = curl_exec($curl);
-           print_r($resp);
+            print_r($resp);
             curl_close($curl);
+            return json_decode($resp, true);
+        } catch (Exception $exception) {
+            echo "Error message ->" . $exception->getMessage() . "->" . $exception->getTraceAsString();
+            return null;
+        }
+    }
+
+
+    public function getScienLabToken($payload)
+    {
+        $url = "https://telco-staging-api.scienlabs.com/api/get_token";
+        try {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Accept: application/json'));
+            $resp = curl_exec($curl);
+            curl_close($curl);
+            print_r($resp);
             return json_decode($resp, true);
         } catch (Exception $exception) {
             echo "Error message ->" . $exception->getMessage() . "->" . $exception->getTraceAsString();
